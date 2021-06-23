53341f6c45ec502e35e6158f82da37149dce3f69
Nick Zincone <nick.zincone@refinitiv.com>
2021-06-22 21:30:17 -0400

    Added for new dev community learning material.

---
+++ src/2. Content/2.1-HistoricalPricing/2.1.03-HistoricalPricing-TimeRange/2.1.03-HistoricalPricing-TimeRange.cs
@@ -0,0 +1,50 @@
+﻿using Common_Examples;
+using Refinitiv.Data.Content.HistoricalPricing;
+using Refinitiv.Data.Core;
+using System;
+
+namespace _2._1._03_HistoricalPricing_TimeRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Create the platform session.
+                using ISession session = Configuration.Sessions.GetSession();
+
+                // Open the session
+                session.Open();
+
+                // Daily summaries for the last 30 days. Start and end dates must be UTC formatted
+                // Note: The days reported only include trading days.
+                var last_30_days = DateTime.UtcNow.AddDays(-30);
+                Console.WriteLine($"Date: {last_30_days}");
+                var response = Summaries.Definition("VOD.L").Interval(Summaries.Interval.P1D)
+                                                            .Fields("TRDPRC_1", "LOW_1", "HIGH_1")
+                                                            .Start(last_30_days)
+                                                            .End(DateTime.UtcNow)
+                                                            .GetData();
+
+                Common.DisplayDataSet(response, "Historical daily Summaries - last 30 trading days");
+
+                // Monthly summaries for last calendar year.
+                var lastYear = DateTime.Now.Year - 1;
+                var start = new DateTime(lastYear, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+                var end = new DateTime(lastYear, 12, 31, 0, 0, 0, DateTimeKind.Utc);
+                response = Summaries.Definition("VOD.L").Interval(Summaries.Interval.P1M)
+                                                        .Fields("TRDPRC_1", "LOW_1", "HIGH_1")
+                                                        .Start(start)
+                                                        .End(end)
+                                                        .GetData();
+
+                Common.DisplayDataSet(response, "Historical monthly Summaries - last calendar year");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"\n**************\nFailed to execute: {e.Message}\n{e.InnerException}\n***************");
+            }
+        }
+    }
+}
---
+++ src/2. Content/2.2-Pricing/2.2.07-Pricing-StreamingSnapshot/2.2.07-Pricing-StreamingSnapshot.cs
@@ -0,0 +1,57 @@
+﻿using Refinitiv.Data.Content.Pricing;
+using Refinitiv.Data.Core;
+using Refinitiv.Data.Delivery.Stream;
+using System;
+
+namespace _2._2._07_Pricing_StreamingSnapshot
+{
+    // **********************************************************************************************************************
+    // 2.2.07-Pricing-StreamingSnapshot
+    // The following example demonstrates a basic use case of the Pricing interface to retrieve a snapshot of prices from
+    // the streaming services available within the platform.  The interface supports the ability to specify a list of items
+    // and the fields for each to retrieve.
+    //
+    // Suggestion: This example uses streaming services.  For users that do not have access to streaming services, but do
+    //             have access to the snapshot pricing service within RDP, you can refer to the 2.2.01-Pricing-Snapshot
+    //             example.
+    //
+    // Note: To configure settings for your environment, visit the following files within the .Solutions folder:
+    //      1. Configuration.Session to specify the access channel into the platform. Default: RDP (PlatformSession).
+    //      2. Configuration.Credentials to define your login credentials for the specified access channel.
+    // **********************************************************************************************************************
+    class Program
+    {
+        static void Main(string[] _)
+        {
+            try
+            {
+                // Create a session into the platform...
+                using ISession session = Configuration.Sessions.GetSession();
+
+                // Open the session
+                session.Open();
+
+                // Specify a list of items and fields to retrieve. Retrieve the stream, indicating a snapshot, i.e. streaming=false.
+                using var stream = Pricing.Definition("EUR=", "CAD=", "USD=").Fields("DSPLY_NAME", "BID", "ASK")
+                                                                             .GetStream().Streaming(false);
+
+                stream.Open();
+
+                // Print 1 field
+                Console.WriteLine($"\nEUR=[DSPLY_NAME]: {stream["EUR="]["DSPLY_NAME"]}");
+
+                // Print the contents of one item
+                Console.WriteLine($"\nEUR= contents: {stream["EUR="].Fields()}");
+
+                // Iterate through the response and print out specific fields for each entry
+                Console.WriteLine("\nIterate through the cache and display a couple of fields");
+                foreach (var item in stream)
+                    Console.WriteLine($"Quote for item: {item.Key}\n{item.Value.Fields("BID", "ASK")}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"\n**************\nFailed to execute: {e.Message}\n{e.InnerException}\n***************");
+            }
+        }
+    }
+}
